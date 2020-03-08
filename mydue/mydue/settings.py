d4f185885cc8dbf0e9a2ad88311e35e047b72478
MUHAMMED YASEEN N A <36787696+121yaseen@users.noreply.github.com>
2020-03-08 07:50:24 +0530

    Login & Reg

--- mydue/mydue/settings.py
+++ mydue/mydue/settings.py
@@ -81,15 +81,10 @@ WSGI_APPLICATION = 'mydue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mytest',
-        'USER':'root',
-        'PASSWORD':'password',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
